c16df52b8aff65fa039dd89e70ab3953282c33d2
183237 <awesomex005@gmail.com>
2020-03-01 15:42:29 +0800

    add code to inspect the distribution of the size of the gtbboxes.

--- README.md
+++ README.md
@@ -5,7 +5,10 @@ python inspect_data.py --data_path ../insects/train/
 ```
 valid Images: 1693 total objects: 10347
 objs per image [max, avg, min]: [10, 6.1116361488481985, 4]
+Generate image: compare_gtbbox_area_to_image_area.png
 ```
+![compare_gtbbox_area_to_image_area-train](compare_gtbbox_area_to_image_area-train.png)
+
 python inspect_data.py --data_path ../insects/val/
 ```
 valid Images: 245 total objects: 1856
--- inspect_data.py
+++ inspect_data.py
@@ -1,8 +1,21 @@
+import matplotlib
+matplotlib.use('Agg')
+
+import numpy as np
+import matplotlib.pyplot as plt
 from insects_reader import get_insect_names, get_annotations
 import argparse
 
-def data_summary(datadir):
-    records = get_annotations(get_insect_names(), datadir)
+def x_histograms(data, num_bins, name):
+    fig = plt.figure(figsize=(8,6))
+    n, bins, patches = plt.hist(data, num_bins, density=1, facecolor='blue', alpha=0.5)
+    plt.title(name)
+    plt.savefig("%s.png" %name)
+    print("Generate image: %s.png" %name)
+    plt.show()
+    return
+
+def data_summary(records):
     valid_images = len(records)
     total_objects = 0
     max_objs_per_img = 0
@@ -16,6 +29,21 @@ def data_summary(datadir):
     avg_objs_per_img = total_objects*1.0/valid_images
     print("valid Images: {} total objects: {}".format(valid_images, total_objects))
     print("objs per image [max, avg, min]: {}".format([max_objs_per_img, avg_objs_per_img, min_objs_per_img]))
+    return
+
+def inspect_gtbboxes(records):
+    gtbbox_area_scales = []
+    for record in records:
+        img_w = record["w"]
+        img_h = record["h"]
+        for gtbbox in record["gt_bbox"]:
+            box_w = gtbbox[2]
+            box_h = gtbbox[3]
+            area_scale = (box_w*box_h)*1.0/(img_w*img_h)
+            gtbbox_area_scales.append(area_scale)
+
+    x_histograms(gtbbox_area_scales, 200, "compare_gtbbox_area_to_image_area")
+    return
 
 def parse_args():
     parser = argparse.ArgumentParser("Data summary")
@@ -30,4 +58,7 @@ def parse_args():
 args = parse_args()
 
 if __name__ == '__main__':
-    data_summary(args.data_path)
+    records = get_annotations(get_insect_names(), args.data_path)
+    data_summary(records)
+    inspect_gtbboxes(records)
+
